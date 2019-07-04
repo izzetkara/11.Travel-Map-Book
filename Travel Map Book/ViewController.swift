7eafb912a13d67d02c0ebb563144d97abe237aa1
İzzet Kara <izzet26kara@gmail.com>
2019-07-04 18:24:53 +0300

    Update: Location of user.

--- Travel Map Book/ViewController.swift
+++ Travel Map Book/ViewController.swift
@@ -8,18 +8,35 @@
 
 import UIKit
 import MapKit
-class ViewController: UIViewController,MKMapViewDelegate {
+import CoreLocation
+
+class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager() //Kullanicinin nerede oldugunu bulmaya calisicaz.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //Best secercek en iyi sekilde konumu bulur fakat pil cok harcar.
+        locationManager.requestWhenInUseAuthorization() // kullanildiginda konumu takip eder.
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view.
     }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        //Asagida ne kadar zoom yapacagimizi belirtiyoruz.
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
     
     
 }
